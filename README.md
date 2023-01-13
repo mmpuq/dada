0bdbcfb569c3831436a9de3692b467bcdfe866b2
hrzyang <zyphp@vip.qq.com>
2023-01-13 19:59:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+adaptable测试
---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+var exec = require("child_process").exec;
+const os = require("os");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.send("hello wolrd");
+});
+
+//获取系统进程表
+app.get("/status", (req, res) => {
+  let cmdStr = "ps -ef";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>命令行执行结果：\n" + stdout + "</pre>");
+    }
+  });
+});
+
+//启动web（xray-core）
+app.get("/start", (req, res) => {
+  let cmdStr = "chmod +x ./web && ./web -c ./config.yaml >/dev/null 2>&1 &";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    } else {
+      res.send("命令行执行结果：" + "启动成功!");
+    }
+  });
+});
+
+//获取系统版本、内存信息
+app.get("/info", (req, res) => {
+  let cmdStr = "cat /etc/*release | grep -E ^NAME";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("命令行执行错误：" + err);
+    } else {
+      res.send(
+        "命令行执行结果：\n" +
+          "Linux System:" +
+          stdout +
+          "\nRAM:" +
+          os.totalmem() / 1000 / 1000 +
+          "MB"
+      );
+    }
+  });
+});
+
+//文件系统只读测试
+app.get("/test", (req, res) => {
+  fs.writeFile("./test.txt", "这里是新创建的文件内容!", function (err) {
+    if (err) res.send("创建文件失败，文件系统权限为只读：" + err);
+    else res.send("创建文件成功，文件系统权限为非只读：");
+  });
+});
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
+    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
+    ws: true, // 是否代理websockets
+    pathRewrite: {
+      // 请求中去除/api
+      "^/api": "/qwe",
+    },
+    onProxyReq: function onProxyReq(proxyReq, req, res) {},
+  })
+);
+
+/* keepalive  begin */
+/*
+function keepalive() {
+  // 1.请求主页，保持唤醒
+  let render_app_url = "https://clever-twill-dove.cyclic.app";
+  request(render_app_url, function (error, response, body) {
+    if (!error) {
+      console.log("主页发包成功！");
+      console.log("响应报文:", body);
+    } else console.log("请求错误: " + error);
+  });
+
+  // 2.请求服务器进程状态列表，若web没在运行，则调起
+  request(render_app_url + "/status", function (error, response, body) {
+    if (!error) {
+      if (body.indexOf("./web -c ./config.yaml") != -1) {
+        console.log("web正在运行");
+      } else {
+        console.log("web未运行,发请求调起");
+        request(render_app_url + "/start", function (err, resp, body) {
+          if (!err) console.log("调起web成功:" + body);
+          else console.log("请求错误:" + err);
+        });
+      }
+    } else console.log("请求错误: " + error);
+  });
+}
+setInterval(keepalive, 9 * 1000);
+*/
+/* keepalive  end */
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
